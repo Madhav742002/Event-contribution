27a00125aaea2fe5e0afd7b874f9ce1f84e289c7
pappu-DD <codeactionwala@gmail.com>
2025-05-27 00:14:02 +0530

    auth background /50

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center mt-6">
+    <div className="flex justify-center p-6 bg-black/40">
       <SignIn>
         <h1>coustom</h1>
       </SignIn>
